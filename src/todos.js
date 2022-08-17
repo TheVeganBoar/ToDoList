b4c75f5ce1f738fb83a7c19618e1aa76527b5719
Juan Cruz <juancruzpalau@gmail.com>
2022-08-17 14:16:43 -0300

    Setup todo items Class

---
+++ src/todos.js
@@ -0,0 +1,9 @@
+export default class Todo {
+    constructor (id, title, description, due_date, priority) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.due_date = due_date;
+        this.priority = priority;
+    }
+};
