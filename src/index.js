d4fd3c58e04b5bc88850862db1276492f97aa6f9
Juan Cruz <juancruzpalau@gmail.com>
2022-08-17 12:39:09 -0300

    Testing bundle output

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const contentDiv = document.querySelector('#content')
+
+const testHI = document.createElement('h1')
+
+testHI.textContent = 'Hello World, my name is THE VEGAN BOAR';
+contentDiv.appendChild(testHI)
