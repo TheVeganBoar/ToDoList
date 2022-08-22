a644a5f5dcd1c7b005d6da2f8d1ed6ad777fca28
Juan Cruz <juancruzpalau@gmail.com>
2022-08-22 18:05:47 -0300

    Adds funciton that generates modal to create new projects (modal includes form with project name input and submit input)

---
+++ src/newProjectModal.js
@@ -0,0 +1,25 @@
+export function generateProjectModal(){
+    let modal = document.createElement('div')
+    modal.id = 'project-modal'
+    modal.className = 'hidden'
+
+    let form = document.createElement('form')
+    form.id = 'new-project-form'
+
+    let input = document.createElement('input')
+    input.type = 'text'
+    input.name = 'project'
+    input.id = 'project-input'
+    input.placeholder = 'Project name...'
+    input.maxlenght = 20
+    input.required = true
+
+    let submit = document.createElement('input')
+    submit.type = 'submit'
+    submit.value = 'SUBMIT'
+    submit.id = ' project-submit'
+
+    form.appendChild(input)
+    form.appendChild(submit)
+    modal.appendChild(form)
+}
