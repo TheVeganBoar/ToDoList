d6c89ddd2e1b1e1350cdfd7a5d4c32c1885df287
Juan Cruz <juancruzpalau@gmail.com>
2022-08-24 14:36:56 -0300

    Corrects elements creations. Adds properties to elements. Creates priority options.

--- src/newItemModal.js
+++ src/newItemModal.js
@@ -1,9 +1,8 @@
 export function generateItemModal(){
     let modal = document.createElement('div')
     modal.id = 'item-modal'
-    modal.className = ''
+    modal.className = 'hidden'
 
-    //task div, h3, title, description, due date, priority, done button
     let modalContent = document.createElement('div')
     modalContent.className = 'modal-content'
     
@@ -12,27 +11,62 @@ export function generateItemModal(){
     h3newTask.textContent = 'NEW TASK'
     modalContent.appendChild(h3newTask)
 
-    let form = document.createElement('form')
-    form.id = 'form'
+    let itemModalForm = document.createElement('form')
+    itemModalForm.id = 'itemModal-form'
 
-    let title = document.createElement('div')
+    let title = document.createElement('input')
+    title.required = true
+    title.placeholder = 'Title'
+    title.maxLength = 25
+    title.className = 'formItem title'
 
-    let description = document.createElement('div')
+    let description = document.createElement('textarea')
+    description.placeholder = 'Description'
+    description.className = 'formItem description'
 
     let due_date = document.createElement('input')
     due_date.type = 'date'
+    due_date.name = 'duedate'
+    due_date.className = 'formItem duedate'
 
-    let priority = document.createElement('button')
+    let dueDateLabel = docuemnt.createElement('label')
+    dueDateLabel.for = 'duedate'
 
-    let doneButton = document.createElement('input')
-    doneButton.type = 'submit'
-    doneButton.value = 'SUBMIT'
-    doneButton.id = 'item-submit'
+    let priority = document.createElement('select')
+    priority.className = 'hidden-priority'
+    // Uses loop to create three priority "boxes" and attaches them to select element
+    for (let i=0 ; i<=2 ; i++) {
+        let box = document.createElement('option')
+        box.value = i
+        priority.appendChild(box)
+    }
 
-    form.appendChild(title)
-    form.appendChild(description)
-    form.appendChild(due_date)
-    form.appendChild(priority)
-    form.appendChild(doneButton)
-    modalContent.appendChild(form)
+    priority.style.display = 'none'
+
+    // Creates option-div and adds classes to style options
+    let priorityButtons = document.createElement('div')
+    priorityButtons.className = `formItem select-priority`
+    priorityButtons.innerHTML = `
+        <label id="priority-label"> Priority </label>
+        <span class="priority-option" id="priority-1"></span>
+        <span class="priority-option darken" id="priority-2"></span>
+        <span class="priority-option darken" id="priority-3"></span>
+        `
+
+    let createButton = document.createElement('input')
+    createButton.type = 'submit'
+    createButton.value = 'CREATE'
+    createButton.className = 'formItem submit'
+    createButton.id = 'submit'
+
+    itemModalForm.appendChild(title)
+    itemModalForm.appendChild(description)
+    itemModalForm.appendChild(dueDateLabel)
+    itemModalForm.appendChild(due_date)
+    itemModalForm.appendChild(priority)
+    itemModalForm.appendChild(priorityButtons)
+    itemModalForm.appendChild(doneButton)
+
+    modalContent.appendChild(itemModalForm)
+    modal.appendChild(modalContent)
 }
